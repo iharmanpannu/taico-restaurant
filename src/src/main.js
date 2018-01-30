1ad8358a8f02f1aa8fe4627cf709961c45d744fb
iharmanpannu <iharmanpannu@gmail.com>
2018-01-30 02:13:36 -0800

    deleted scss and other files changed the whole look

---
+++ src/src/main.js
@@ -0,0 +1,18 @@
+AOS.init({ duration: 1000 });
+
+const nav = $("#mobile-nav");
+const closeBtn = $("#close-tog");
+const openBtn = $("#open-tog");
+$(nav).hide();
+
+$(openBtn).on("click", () => {
+  if ($(window).width() <= 786) {
+    $(nav)
+      .slideDown(500)
+      .show();
+  }
+});
+
+$(closeBtn).on("click", () => {
+  $(nav).slideUp(300);
+});
